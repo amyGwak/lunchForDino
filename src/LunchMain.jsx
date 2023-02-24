ce41f9e552715ef41b436810d4c58c40a2ee6ee2
amyGwak <amy@nonegolab.com>
2023-02-24 14:36:40 +0900

    animation 추가~

--- src/LunchMain.jsx
+++ src/LunchMain.jsx
@@ -1,6 +1,9 @@
 import React, {useState} from "react";
 import {detailMenu} from "./menu";
-import styled from 'styled-components';
+import styled, {keyframes} from 'styled-components';
+import bonobono from "./asset/dinobono.jpeg";
+import amy from "./asset/amy.png";
+import dinoCrying from "./asset/dinocrying.jpeg";
 
 
 const LunchMain = () => {
@@ -9,35 +12,56 @@ const LunchMain = () => {
     const [curSub, setCurSub] = useState(null);
     const [selectedList, setSelectedList] = useState([]);
     const [selectedMenu, setSelectedMenu] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const [isHover, setIsHover] = useState(false);
 
     const onClickMainMenu = (menu) => {
         setCurMain(menu);
         setCurSub(null);
+        setSelectedMenu("");
     }
 
     const onClickCancelSelect = (menu) => {
         setSelectedList(selectedList.filter((m) => m !== menu));
+        setSelectedMenu("");
     }
 
     const onClickSelectMenu = (menu) => {
         if(curSub !== null && !selectedList.includes(menu)) {
           setSelectedList([...selectedList, menu]);
+          setSelectedMenu("");
         }
     }
 
     const onClickRandomGame = () => {
+      if(selectedList.length === 0) {
+        return;
+      }
+      setIsLoading(true);
+
       const randomIdx = Math.floor(Math.random() * selectedList.length);
-      setSelectedMenu(selectedList[randomIdx])
+      setSelectedMenu(selectedList[randomIdx]);
+
+      setTimeout(() => setIsLoading(false), 3000);
+
+    }
+
+    const onClickReset = () => {
+      setSelectedList([]);
+      setSelectedMenu("");
     }
 
     return (
         <div>
-            <Title><span>디노</span>의 점심 메뉴 고르기를 도와주세요!!!</Title>
+            <Title>
+              <img src={dinoCrying} alt={"dino"}/>의 점심 메뉴 고르기를 도와주세요!!!
+            </Title>
             <MenuCategory>
                 {
                     Object.keys(detailMenu).map((menu) => {
                         return (
                             <Menu
+                                key={menu}
                                 isSelected={menu === curMain}
                                 onClick={() => onClickMainMenu(menu)}>
                                 {menu}
@@ -65,7 +89,8 @@ const LunchMain = () => {
                     curSub !== null &&
                     detailMenu[curMain][curSub]?.map((menu) => {
                         return (
-                            <Card onClick={() => onClickSelectMenu(menu)}>
+                            <Card key={menu}
+                                  onClick={() => onClickSelectMenu(menu)}>
                                 {menu}
                             </Card>
                         )
@@ -76,14 +101,14 @@ const LunchMain = () => {
 
 
             <SelectedTitle>
-                디노의 선택 리스트.. <span onClick={() => setSelectedList([])}>리셋</span>
+                디노의 선택 리스트.. <span onClick={onClickReset}>리셋</span>
             </SelectedTitle>
 
             <SelectedList>
                 {
                     selectedList.map((selectedMenu) => {
                         return (
-                            <SelectedMenu>
+                            <SelectedMenu key={selectedMenu}>
                                 {selectedMenu} <span onClick={() => onClickCancelSelect(selectedMenu)}>X</span>
                             </SelectedMenu>
                         )
@@ -91,18 +116,43 @@ const LunchMain = () => {
                 }
             </SelectedList>
 
-            <RandomButton onClick={onClickRandomGame}>
-              1개 뽑기
+          <ButtonArea>
+              <RandomButton onClick={onClickRandomGame}>
+                1개 뽑기
+              </RandomButton>
+              <RandomButton onClick={onClickRandomGame}>
+                다시 뽑기
+              </RandomButton>
+            <RandomButton onClick={() => setSelectedMenu("")}>
+              리셋
             </RandomButton>
+          </ButtonArea>
 
           {
-              selectedMenu !== "" &&
-              <SelectedOne>
-                <span>⭐️{selectedMenu}⭐️</span> 당첨~~~~~~!!
-              </SelectedOne>
+            (selectedMenu !== "" && !isLoading) &&
+                <SelectedOne>
+                  <span>⭐️{selectedMenu}⭐️</span> 당첨~~~~~~!!
+                </SelectedOne>
           }
 
-            <MadeBy>made by 에이미</MadeBy>
+          {
+            isLoading &&
+              <BonoBonoLoading isLoading={isLoading}>
+                <div>아 나 시키지 말라고...</div>
+                <img src={bonobono} alt={"dinobono"}/>
+              </BonoBonoLoading>
+          }
+
+            <MadeBy
+                onMouseOver={() => setIsHover(true)}
+                onMouseOut={() => setIsHover(false)}
+            >
+              made by 에이미
+            </MadeBy>
+          {
+            isHover && <AmyImg src={amy}/>
+          }
+
 
         </div>
     )
@@ -110,9 +160,15 @@ const LunchMain = () => {
 export default LunchMain;
 
 
+const rotate = keyframes`
+  100% {
+    transform: rotate(360deg);
+    }
+`;
+
 const Title = styled.div`
   text-align: center;
-  margin: 100px auto 50px;
+  margin: 80px auto 50px;
   font-size: 28px;
   font-weight: bold;
   
@@ -120,6 +176,13 @@ const Title = styled.div`
     color: rgba(50, 142, 251,1);
   }
   
+  img {
+    width: 80px;
+    
+    &:hover {
+      animation: ${rotate} 2s linear infinite;
+    }
+  }
 `;
 const MenuCategory = styled.div`
   display: flex;
@@ -209,10 +272,10 @@ const SelectedMenu = styled(Card)`
 
 const RandomButton = styled.div`
   text-align: center;
-  margin: 60px auto;
+  margin: 60px 20px 0 0;
   font-size: 16px;
   padding: 20px;
-  width: 140px;
+  width: 150px;
   border: 1px solid black;
   border-radius: 6px;
   cursor: pointer;
@@ -225,12 +288,52 @@ const RandomButton = styled.div`
 
 const SelectedOne = styled.div`
   text-align: center;
-  margin-bottom: 100px;
   font-size: 18px;
+  margin-bottom: 100px;
   span {
     font-weight: bold;
     color: #328EFB;
     font-size: 26px;
+  }
+`;
+
+const ButtonArea = styled.div`
+  display: flex;
+  width: 400px;
+  justify-content: center;
+  margin: 0 auto 40px;
+`;
+
+const byeBonoBono = keyframes`
+  0% {
+    transform: rotate(0deg);
+    opacity: 1;
+  }
+  
+  100% {
+    transform: rotate(350deg);
+    opacity: 0;
+  }
+`;
+
+const BonoBonoLoading = styled.div`
+  margin: 30px auto;
+  text-align: center;
+  animation: ${byeBonoBono} 5s linear;
+  
+  div {
+    font-size: 18px;
+    margin-bottom: 4px;
+  }
+`;
+
+const fadeInToUp = keyframes`
+  0% {
+    transform: translate3d(0, 100%, 0px);
+  }
+  to {
+    bottom: 30px;
+    transform: translateZ(0);
   }
 `;
 
@@ -242,6 +345,14 @@ const MadeBy = styled.div`
   position: fixed;
   bottom: 30px;
   right: 30px;
+`;
+
+const AmyImg = styled.img`
+  width: 100px;
+  position: fixed;
+  bottom: 30px;
+  right: 30px;
+  animation: ${fadeInToUp} 2s 1;
 `;
 
 
